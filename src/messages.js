4c31b20a6aa574cab22ab4db9ed2f9245b6fbd6b
crater44 <dragmos_we@protonmail.com>
2023-08-19 15:27:52 -0500

    program helper refactor

--- src/messages.js
+++ src/messages.js
@@ -1,5 +1,38 @@
 exports.messages = {
   current_token: token => `Your current token is: ${token}`,
-  empty_token: 'Your token is empty',
-  packageDefaultData: (key, value) => `${key} (default: ${value}): `,
+  empty_token: "Your token is empty",
+  packageDefaultDataPrompt: (key, value) => `${key} (default: ${value}): `,
+  required_arg_token: "<token>",
+  required_arg_package: "<pkg>",
+  optional_arg_package: "[pkg]",
+  commands: {
+    set_auth_token: {
+      name: "set-auth-token",
+      description: "Set the auth token\n\n",
+    },
+    get_auth_token: {
+      name: "get-auth-token",
+      description: "Display the current token\n\n"    
+    },
+    init: {
+      name: "init",
+      description: "Create a greypackage.json file to write down the installed dependencies\n\n"
+    },
+    list: {
+      name: "list",
+      description: "List all the github repositories containing the file greypackage.json\n\n"
+    },
+    description: {
+      name: "description",
+      description: "Show the description of a package\n\n",
+    },
+    author: {
+      name: "author",
+      description: "Show the name of the author of a package\n\n",
+    },
+    install: {
+      name: "install",
+      description: "Install a package into your vendor folder. If the folder doesn't exist, it'll be created.\nAlternatively, when invoked without arguments, the command installs the necessary dependencies.\n\n",
+    },
+  }
 };
--- src/program_helper.js
+++ src/program_helper.js
@@ -1,40 +1,25 @@
 exports.programHelper = {
-  config: function({name, description, version}) {
+  config: function({ name, description, version }) {
     this
     .name(name)
     .description(description)
     .version(version);
   },
-  setCommands: function({fs, constants, messages, userHandler, actions, api, middleware}) {
-    this.command("set-auth-token")
-    .description("Set the auth token\n\n")
-    .action(actions.setAuthToken());
-    
-    this.command("get-auth-token")
-    .description("Display the current token\n\n")
-    .action(actions.getAuthToken({messages: messages, authToken: constants.authToken}));
-    
-    this.command("init")
-    .description("Create a greypackage.json file to write down the installed dependencies\n\n")
-    .action(actions.init({ fs, userHandler }));
-    
-    this.command("list")
-    .description("List all the github repositories containing the file greypackage.json\n\n")
-    .action(actions.list(api));
-    
-    this.command("description")
-    .description("Show the description of a package\n\n")
-    .argument("<pkg>", "package name")
-    .action(actions.getDescription());
-    
-    this.command("author")
-    .description("Show the name of the author of a package\n\n")
-    .argument("<pkg>", "package name")
-    .action(actions.getAuthor());
-    
-    this.command("install")
-    .description("Install a package into your vendor folder. If the folder doesn't exist, it'll be created.\nAlternatively, when invoked without arguments, the command installs the necessary dependencies.\n\n")
-    .argument("[pkg]", "package name")
-    .action(actions.install());
+  setCommands: function({ fs, constants, messages, userHandler, actions, api, middleware }) {
+    const commands = {
+      set_auth_token: { argument: 'required_arg_token', action: () => actions.setAuthToken() },
+      get_auth_token: { action: () => actions.getAuthToken({ messages, authToken: constants.auth}) },
+      init: { action: () => actions.init({ fs, userHandler }) },
+      list: { action: () => actions.list(api) },
+      description: { argument: 'required_arg_package', action: () => actions.getDescription() },
+      author: { argument: 'required_arg_package', action: () => actions.getAuthor() },
+      install: { argument: 'optional_arg_package', action: () => actions.install() },
+    }
+    for (const [name, command] of Object.entries(commands)) {
+        const ctx = this.command(messages.commands[name].name)
+        ctx.description(messages.commands[name].description)
+        ctx.action(command.action())
+        if (command.argument) ctx.arguments(messages[command.argument])
+    }
   },
 }
