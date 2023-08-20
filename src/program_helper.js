1507aa89a393cddf2a0762b95367c3382aec3e67
crater44 <dragmos_we@protonmail.com>
2023-08-20 10:03:29 -0500

    format commands

--- src/program_helper.js
+++ src/program_helper.js
@@ -7,14 +7,40 @@ exports.programHelper = {
   },
   setCommands: function({ fs, constants, messages, userHandler, actions, api, middleware }) {
     const commands = {
-      set_auth_token: { argument: 'required_arg_token', action: () => actions.setAuthToken() },
-      get_auth_token: { action: () => actions.getAuthToken({ messages, authToken: constants.auth}) },
-      init: { action: () => actions.init({ fs, userHandler, messages, pkgData: constants.gbpackage_default_data }) },
-      list: { action: () => actions.list(api) },
-      description: { argument: 'required_arg_package', action: () => actions.getDescription() },
-      author: { argument: 'required_arg_package', action: () => actions.getAuthor() },
-      install: { argument: 'optional_arg_package', action: () => actions.install() },
-    }
+      set_auth_token: {
+        argument: "required_arg_token",
+        action: () => actions.setAuthToken()
+      },
+      get_auth_token: {
+        action: () => actions.getAuthToken({ messages: messages, authToken: constants.auth })
+      },
+      init: {
+        action: () => {
+          actions.init({
+            fs: fs,
+            userHandler: userHandler,
+            messages: messages,
+            pkgData: constants.pkgDefaultData,
+            pkgFileName: constants.pkgFileName,
+          })
+        }
+      },
+      list: {
+        action: () => actions.list(api)
+      },
+      description: {
+        argument: "required_arg_package",
+        action: () => actions.getDescription()
+      },
+      author: {
+        argument: "required_arg_package",
+        action: () => actions.getAuthor()
+      },
+      install: {
+        argument: "optional_arg_package",
+        action: () => actions.install()
+      }
+    };
     for (const [name, command] of Object.entries(commands)) {
         const ctx = this.command(messages.commands[name].name);
         ctx.description(messages.commands[name].description);
